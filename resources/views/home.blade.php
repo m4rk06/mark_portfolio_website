3856fe86405090e761f3d9d59619b1add8c80370
m4rk06 <m4rksarmiento@gmail.com>
2025-11-14 15:41:40 +0800

    port

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+      @vite(['resources/css/mark.css','resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+<body>
+    <h1>
+        AHHHhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+    </h1>
+</body>
+</html>
